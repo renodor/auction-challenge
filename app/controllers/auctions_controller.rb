ac26067ba9bbe5de4e52d875c36cc1c814bdac88
renaud dor <renaud.dor@gmail.com>
2020-05-04 11:22:42 +0200

    create CRUD for auction

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -4,17 +4,45 @@ class AuctionsController < ApplicationController
   end
 
   def show
+    @auction = Auction.find(params[:id])
+  end
+
+  def new
+    @auction = Auction.new
   end
 
   def create
+    @auction = Auction.new(auction_params)
+    if @auction.save
+      redirect_to auction_path(@auction)
+    else
+      render :new
+    end
   end
 
   def edit
+    @auction = Auction.find(params[:id])
   end
 
   def update
+    @auction = Auction.find(params[:id])
+    if @auction.update(auction_params)
+      redirect_to auction_path(@auction)
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @auction = Auction.find(params[:id])
+    @auction.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def auction_params
+    params.require(:auction).permit(:title, :content, :price)
   end
 end
