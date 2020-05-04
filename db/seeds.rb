ca247b3421bd945933fee72ea531cfa9858c96a0
renaud dor <renaud.dor@gmail.com>
2020-05-04 10:37:38 +0200

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis officiis expedita cupiditate ipsa accusamus eius possimus porro vero perspiciatis ipsum nulla excepturi, non, rem numquam quasi similique facere et deleniti."
+
+p 'Destroy all auctions'
+Auction.destroy_all
+p '----------> done'
+
+10.times do |n|
+  Auction.create!(title: Faker::DcComics.hero, content: content, price: rand(50) + rand.round(2))
+  p "auction #{n + 1} created!"
+end
+
+p "----------> done, #{Auction.count} auctions created"
